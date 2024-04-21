41ee05be91ccc5a7521ce75957e2ef4ebde5ff87
cadetsaad <cadetsaadsoomro@gmail.com>
2024-04-21 14:10:26 +0500

    Modified All

---
+++ src/Levels.js
@@ -0,0 +1,13 @@
+function Levels() {
+    return (
+        <div>
+            <h1>Select Level</h1>
+            <button> 1 </button>
+            <button> 2 </button>
+            <button> 3 </button>
+            <button> 4 </button>
+        </div>
+    )
+}
+
+export default Levels 
