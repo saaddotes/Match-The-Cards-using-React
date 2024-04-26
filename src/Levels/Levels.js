fd675a392dd4fc98a8d622786c0f448909fbc690
cadetsaad <cadetsaadsoomro@gmail.com>
2024-04-27 02:10:45 +0500

    Updates Levels

--- src/Levels/Levels.js
+++ src/Levels/Levels.js
@@ -1,4 +1,4 @@
-function Levels({currentLevel, setCurrentLevel,unlockedLevles }) {
+function Levels({currentLevel, setCurrentLevel,unlockedLevles}) {
     function levelManager(level) {
         setCurrentLevel(level)
         unlockedLevles.push(level)
@@ -8,10 +8,10 @@ function Levels({currentLevel, setCurrentLevel,unlockedLevles }) {
         <div className="text-center bg-cont rounded pb-3">
             <h1 className="text-white">Select Level</h1>
             <div className="d-flex gap-3 justify-content-center">
-                <button className={`btn btn-secondary ${currentLevel === 0 ? 'bg-primary' : ''} ${unlockedLevles.includes(0) ? 'text-danger' : 'disabled'}`} onClick={() => levelManager(0)}> 1 </button>
-                <button className={`btn btn-secondary ${currentLevel === 1 ? 'bg-primary' : ''} ${unlockedLevles.includes(1) ? 'text-danger' : 'disabled'}`} onClick={() => levelManager(1)}> 2 </button>
-                <button className={`btn btn-secondary ${currentLevel === 2 ? 'bg-primary' : ''} ${unlockedLevles.includes(2) ? 'text-danger' : 'disabled'}`} onClick={() => levelManager(2)}> 3 </button>
-                <button className={`btn btn-secondary ${currentLevel === 3 ? 'bg-primary' : ''} ${unlockedLevles.includes(3) ? 'text-danger' : 'disabled'}`} onClick={() => levelManager(3)}> 4 </button>
+                <button className={`btn btn-secondary ${currentLevel === 0 ? 'bg-primary' : ''} ${unlockedLevles.includes(0) ? 'bg-success' : 'disabled'}`} onClick={() => levelManager(0)}> 1 </button>
+                <button className={`btn btn-secondary ${currentLevel === 1 ? 'bg-primary' : ''} ${unlockedLevles.includes(1) ? 'bg-success' : 'disabled'}`} onClick={() => levelManager(1)}> 2 </button>
+                <button className={`btn btn-secondary ${currentLevel === 2 ? 'bg-primary' : ''} ${unlockedLevles.includes(2) ? 'bg-success' : 'disabled'}`} onClick={() => levelManager(2)}> 3 </button>
+                <button className={`btn btn-secondary ${currentLevel === 3 ? 'bg-primary' : ''} ${unlockedLevles.includes(3) ? 'bg-success' : 'disabled'}`} onClick={() => levelManager(3)}> 4 </button>
             </div>
 
         </div>
